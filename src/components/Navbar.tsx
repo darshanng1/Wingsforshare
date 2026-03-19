6a8e9535f3cfc20afeb54df8a1ca02058d141a22
darshanng1 <96815758+darshanng1@users.noreply.github.com>
2026-03-19 21:18:03 +0530

    Update Navbar.tsx
    logo size adjustment1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -107,7 +107,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <Link to="/" className="flex items-center group">
-            <Logo className="h-16 md:h-20 w-auto" />
+            <Logo className="h-28 md:h-20 w-auto" />
           </Link>
 
           {/* Desktop Menu */}
